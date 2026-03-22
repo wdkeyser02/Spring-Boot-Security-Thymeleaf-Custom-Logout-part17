32a72b4166ea6f455d00006c6e0b4cb7f8fa0e56
Willy De Keyser <spring.boot.programming@gmail.com>
2026-03-22 22:32:12 +0100

    Init

---
+++ Spring-Boot-Security-Thymeleaf-Custom-Logout-part17/src/main/java/willydekeyser/SpringBootSecurityThymeleafCustomLogoutPart17Application.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityThymeleafCustomLogoutPart17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityThymeleafCustomLogoutPart17Application.class, args);
+	}
+
+}
